f0462a91bb8199c2f7ac331779566200bc2ee20d
Galloz97 <71735944+Galloz97@users.noreply.github.com>
2025-11-19 21:43:09 +0100

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -249,7 +249,7 @@ function App() {
   if (!sessionSupabase) {
   return (
     <div className="app-container" style={{alignItems: 'center', marginLeft: 10, marginRight: 10}}>
-    <img src="/vite.png" alt="Logo" style={{height: "350", width: "auto"}} />
+    <img src="/vite.png" alt="Logo" style={{height: "200", width: "auto"}} />
       <div className="card">
         <Auth
           supabaseClient={supabase}
